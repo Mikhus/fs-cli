c776e279b6801c618bba8af6aa813c3681ae4ec6
Mykhailo Stadnyk <mike.stadnyk@sisense.com>
2015-12-01 21:23:24 +0200

    tests updated

---
+++ test/unzip.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var fs = require('../index');
+var tgz = require('tar.gz');
+var p = require('path');
+
+describe('fs.unzip()', function () {
+    before(function () {
+        fs.error(null);
+        fs.zip('./test', './tmp/test.zip');
+        assert.equal(fs.error(), null);
+    });
+
+    it('should unzip given archive', function () {
+        assert.equal(fs.unzip('./tmp/test.zip', './tmp'), true);
+        assert.equal(fs.error(), null);
+    });
+    it('destination should have correct list of files', function () {
+        assert.deepEqual(fs.ls('./test').sort(), fs.ls('./tmp/test').sort());
+        assert.equal(fs.error(), null);
+    });
+
+    after(function() {
+        assert.equal(fs.rm('./tmp'), true);
+    });
+});
