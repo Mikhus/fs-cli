d3789662f4571d2d757c2096a077b2c0649a3ccf
Mykhailo Stadnyk <mike.stadnyk@sisense.com>
2015-12-01 18:55:15 +0200

    mkdir() test update (removing fail check due to on linux almost impossible to fail dir cratation via name, it accepts almost everything, so there should be found some another method later)

--- test/mkdir.js
+++ test/mkdir.js
@@ -13,12 +13,6 @@ describe('fs.mkdir()', function () {
     it('path should be really created', function () {
         assert.equal(!!fs.exists('./test/some/path/depth'), true);
     });
-    it('should return false on fail', function () {
-        assert.equal(fs.mkdir('./test/*/**/***'), false);
-    });
-    it('should report error properly', function () {
-       assert.equal(fs.error() instanceof Error, true);
-    });
 
     after(function() {
         assert.equal(fs.rm('./test/some'), true);
